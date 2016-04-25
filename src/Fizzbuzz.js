82fe49129a05b04aa24fc6b491b3356639f7c0fa
Efremov Alexey <lexich121@gmail.com>
2016-04-25 16:14:39 +0300

    Add: mocha support with eslint & editorconfig

--- src/Fizzbuzz.js
+++ src/Fizzbuzz.js
@@ -1,31 +1,28 @@
-IDontKnow = "";
+/* eslint no-console: 0 */
+var IDontKnow = "";
 
 /**
 Too many methods. Encapsulate them in a data structure
 */
 function valueFilter(value, number, result, previous) {
-    if ((value % number) == 0)
-        return previous + result;
-    return previous;
+  return (value % number) === 0 ? (previous + result) : previous;
 }
 
-
 function numFilter(value, result) {
-    if (result == IDontKnow)
-        return value;
-    return result;
+  return result === IDontKnow ? value : result;
 }
 
 function fizbuzzTo(limit) {
-    values = []
-    for (var i = 1; i <= limit; i++) {
-        result = valueFilter(i, 3, "Fizz", IDontKnow);
-        result = valueFilter(i, 5, "Buzz", result);
-        result = valueFilter(i, 7, "Whizz", result);
-        result = numFilter(i, result);
-        values.push(result);
-    };
-    return values;
+  var values = [];
+  var result;
+  for (var i = 1; i <= limit; i++) {
+    result = valueFilter(i, 3, "Fizz", IDontKnow);
+    result = valueFilter(i, 5, "Buzz", result);
+    result = valueFilter(i, 7, "Whizz", result);
+    result = numFilter(i, result);
+    values.push(result);
+  }
+  return values;
 }
 
 /**
@@ -33,22 +30,30 @@ function fizbuzzTo(limit) {
 	I use this version to do mockey patch and mocking the console during testing
 **/
 
-function fizzbuzz_toconsole() {
-    function generate(input) {
-        return ((fizz(input) && buzz(input) && 'FizzBuzz') ||
-            fizz(input) || buzz(input)) || input;
-
-        function fizz(input) {
-            return ((input % 3) === 0) ? 'Fizz' : false;
-        }
-
-        function buzz(input) {
-            return ((input % 5) === 0) ? 'Buzz' : false;
-        }
+function fizzbuzzToconsole () {
+  function generate(input) {
+    function fizz(input) {
+      return ((input % 3) === 0) ? "Fizz" : false;
     }
 
-    var i;
-    for (i = 1; i <= 5; i++) {
-        console.log(generate(i));
+    function buzz(input) {
+      return ((input % 5) === 0) ? "Buzz" : false;
     }
+
+    return ((fizz(input) && buzz(input) && "FizzBuzz") ||
+      fizz(input) || buzz(input)) || input;
+  }
+
+  var i;
+  for (i = 1; i <= 5; i++) {
+    console.log(generate(i));
+  }
 }
+
+/* eslint object-shorthand: 0 */
+module.exports = {
+  valueFilter: valueFilter,
+  numFilter: numFilter,
+  fizbuzzTo: fizbuzzTo,
+  fizzbuzz_toconsole: fizzbuzzToconsole
+};
