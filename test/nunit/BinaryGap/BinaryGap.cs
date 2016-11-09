40f3df918cc8ccd116125cb6db778b21fa5432a2
robyy666 <ya.minor3000@yandex.ru>
2016-11-09 09:46:16 +0300

    Added BinaryGap kata, solution and tests

---
+++ test/nunit/BinaryGap/BinaryGap.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.BinaryGap
+{
+    static class BinaryGap
+    {
+        const int TwoPowerOfIntMax = 32;
+
+        public static int GetGapSize(int number)
+        {
+            bool started = false;
+            int maxGapLenght = 0;
+            int currentGapLength = 0;
+
+            for (int caret = 0; caret < TwoPowerOfIntMax; caret++)
+            {
+                int mask = 1 << caret;
+                if ((number & mask) == mask)
+                {
+                    if (started)
+                    {
+                        maxGapLenght = Math.Max(maxGapLenght, currentGapLength);
+                        currentGapLength = 0;
+                    }
+                    else started = true;
+                }
+                else if (started)
+                {
+                    currentGapLength++;
+                }
+            }
+            return maxGapLenght;
+        }
+    }
+}
---
+++ test/nunit/BinaryGap/BinaryGapTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.BinaryGap
+{
+    [TestFixture]
+    [Category("BinaryGapKata")]
+    class BinaryGapTest
+    {
+        Dictionary<int, int> testTable;
+
+        [SetUp]
+        public void InitializeTestTable()
+        {
+            testTable = new Dictionary<int, int>
+            {
+                { 9,    2 },
+                { 529,  4 },
+                { 20,   1 },
+                { 15,   0 }
+            };
+        }
+
+        [Test]
+        public void BinaryGap_TableTest()
+        {
+            foreach (var pair in testTable)
+            {
+                Assert.AreEqual(pair.Value, BinaryGap.GetGapSize(pair.Key));
+            }
+        }
+    }
+}
