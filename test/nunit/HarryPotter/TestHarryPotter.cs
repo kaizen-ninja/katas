507281441842817e75872f03ada6bedb257d7413
robyy666 <ya.minor3000@yandex.ru>
2016-11-09 08:14:36 +0300

    Renamed HarryPotter namespace, added HarryPotter kata realization

--- test/nunit/HarryPotter/TestHarryPotter.cs
+++ test/nunit/HarryPotter/TestHarryPotter.cs
@@ -1,21 +1,67 @@
 ﻿using NUnit.Framework;
+using System.Collections.Generic;
 
-namespace Katas.TheHarryPoterKata
+namespace Katas.TheHarryPotterKata
 {
     [TestFixture]
     [Category("HarryPoterKata")]
-    public class TestHarryPoter
+    public class TestHarryPotter
     {
-        [TestFixtureSetUp]
-        public void Setup()
+        readonly float[] Discounts = { 1.00f, 0.95f, 0.90f, 0.80f, 0.75f };
+        const float BookPrice = 8f;
+
+        HarryPotterShop shop;
+
+        [SetUp]
+        public void Harry_Setup()
         {
-            //Setup
+            shop = new HarryPotterShop(BookPrice, Discounts);
         }
 
-        [TestFixtureTearDown]
-        public void TearDown()
+        [Test]
+        public void Harry_TestOne()
         {
-            //TearDown
+            float sum = shop.GetBasketSum(2, 2, 2, 1, 1);
+            Assert.AreEqual(51.6f, sum);
+        }
+
+        [Test]
+        public void Harry_TestTwo()
+        {
+            float sum = shop.GetBasketSum();
+            Assert.AreEqual(0.0f, sum);
+            sum = shop.GetBasketSum(0);
+            Assert.AreEqual(0.0f, sum);
+            sum = shop.GetBasketSum(0, 0, 0, 0);
+            Assert.AreEqual(0.0f, sum);
+        }
+
+        [Test]
+        public void Harry_TestThree()
+        {
+            float sum = shop.GetBasketSum(2);
+            Assert.AreEqual(16.0f, sum);
+        }
+
+        [Test]
+        public void Harry_TestDiscounts()
+        {
+            var discountTests = new Dictionary<int, float>()
+            {
+                {-1, 0f     },
+                { 0, 0f     },
+                { 1, 1f     },
+                { 2, 0.95f  },
+                { 3, 0.90f  },
+                { 4, 0.80f  },
+                { 5, 0.75f  },
+                { 6, 0.75f  },
+                { 25, 0.75f }
+            };
+            foreach (var pair in discountTests)
+            {
+                Assert.AreEqual(pair.Value, shop.GetDiscount(pair.Key));
+            }
         }
     }
 }
