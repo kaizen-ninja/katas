226c9d62918b7026d076b22f73874e309e86c0b3
robyy666 <ya.minor3000@yandex.ru>
2016-11-09 13:42:45 +0300

    Added Reverse Polish Notation kata, solution and tests

---
+++ test/nunit/ReversePolishNotation/ReversePolishNotation.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Katas.ReversePolishNotation
+{
+    class ReversePolishNotationSession
+    {
+        string expression;
+
+        int caret = 0;
+
+        char currentChar { get { return expression[caret]; } }
+        bool eof { get { return caret >= expression.Length; } }
+
+        Dictionary<char, Func<double, double, double>> operationsTable;
+
+        Stack<double> numbers;
+        Stack<Func<double, double, double>> operations;
+
+        public ReversePolishNotationSession(string expression)
+        {
+            this.expression = expression;
+            numbers = new Stack<double>();
+            operations = new Stack<Func<double, double, double>>();
+
+            operationsTable = new Dictionary<char, Func<double, double, double>>
+            {
+                { '+', (a, b) => a + b },
+                { '-', (a, b) => a - b },
+                { '*', (a, b) => a * b },
+                { '/', (a, b) => a / b }
+            };
+        }
+
+        public double Evaluate()
+        {
+            caret = 0;
+
+            while(!eof)
+            {
+                SkipWhitespaces();
+                if (eof) break;
+
+                if (char.IsDigit(currentChar))
+                {
+                    ProceedStack();
+                    numbers.Push(ResolveNumber());
+                }
+                else
+                {
+                    var op = ResolveOperation();
+                    ProceedStack();
+                    operations.Push(op);
+                    ProceedStack();
+                }
+            }
+            ProceedStack();
+
+            return numbers.Pop();
+        }
+        void ProceedStack()
+        {
+            while (operations.Count > 0)
+            {
+                numbers.Push(operations.Pop()(numbers.Pop(), numbers.Pop()));
+            }
+        }
+
+        double ResolveNumber()
+        {
+            int started = caret;
+            do
+            {
+                caret++;
+            } while (!eof && char.IsDigit(currentChar));
+
+            return double.Parse(expression.Substring(started, caret - started));
+        }
+
+        void SkipWhitespaces()
+        {
+            while (!eof && char.IsWhiteSpace(currentChar))
+                caret++;
+        }
+
+        Func<double, double, double> ResolveOperation()
+        {
+            Func<double, double, double> op;
+            if (!operationsTable.TryGetValue(currentChar, out op))
+                throw new Exception("Can't resolve expression, invalid symbol " + currentChar);
+            caret++;
+            return op;
+        }
+    }
+}
---
+++ test/nunit/ReversePolishNotation/TestReversePolishNotation.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Katas.ReversePolishNotation
+{
+    [TestFixture]
+    [Category("ReversePolishNotation")]
+    class TestReversePolishNotation
+    {
+        Dictionary<string, double> testsTable;
+
+        [SetUp]
+        public void InitializeTestTable()
+        {
+            testsTable = new Dictionary<string, double>
+            {
+                { "5", 5 },                 //5
+                { "2 3 +", 2 + 3 },         //5
+                { "3 4 - 5 +", 4 - 3 + 5 }, //6
+                { "3 4 5 * -", 5 * 4 - 3 }  //17
+            };
+        }
+
+        [Test]
+        public void RPN_One()
+        {
+            foreach (var test in testsTable)
+            {
+                var result = new ReversePolishNotationSession(test.Key).Evaluate();
+                Assert.AreEqual(test.Value, result);
+            }
+        }
+    }
+}
