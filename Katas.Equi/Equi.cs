28aadd9d65851db28ef3191c3463dfd0622116df
VadimVerhovskyi <tabtyjka@gmail.com>
2016-12-11 01:21:25 +0300

    Solution of Equie kata.

---
+++ Katas.Equi/Equi.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace EquiKata
+{
+    public class Equi
+
+    {
+        public int[] array;
+
+
+        public Equi(int[] array)
+        {
+            this.array = array;
+
+        }
+        public bool IsEquilibrium(int indexEl)
+        {
+            int summ1 = 0;
+            int summ2 = 0;
+            for (int i = 0; i < indexEl; i++)
+            {
+                summ1 += array[i];
+            }
+            for (int i = indexEl + 1; i < array.Length; i++)
+            {
+                summ2 += array[i];
+            }
+            return summ1 == summ2;
+
+
+        }
+        public List<int> GetIndexOfEquilibrium()
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (IsEquilibrium(i))
+                {
+                    result.Add(i);
+                }
+            }
+
+
+            return result.Count == 0 ? new List<int>() { -1 } : result;
+        }
+    }
+}
+
---
+++ Katas.Equi/TestEqui.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace EquiKata
+{
+    [TestFixture]
+    public class TestEqui
+    {
+        Equi Eq = new Equi(new int[] { -1, -2, 4, -6, 1 });
+
+        [Test]
+        public void TestCreateEkz()
+        {
+            Assert.IsNotNull(Eq);
+        }
+        [Test]
+        public void TestIsEquilibrium()
+        {
+            Assert.IsTrue(Eq.IsEquilibrium(3));
+            Assert.IsTrue(Eq.IsEquilibrium(1));
+        }
+        [Test]
+        public void TestGetIndexOfEq()
+        {
+            Assert.AreEqual(new List<int>() {1,3}, Eq.GetIndexOfEquilibrium());
+        }
+    }
+}
