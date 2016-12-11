276cf6110eb560457c002454003c3f7e1721b9ba
robyy666 <ya.minor3000@yandex.ru>
2016-12-12 00:56:46 +0300

    Added solution for Equi kata.

---
+++ test/nunit/Equi/TestEqui.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Katas.Equi
+{
+    class TestEqui
+    {
+        Equi equi = new Equi();
+
+        [Test]
+        public void ExistsTest()
+        {
+            Assert.IsNotNull(equi);
+        }
+
+        [Test]
+        public void DivideSumTest()
+        {
+            var arr = new int[] { -1, 3, -4, 5, 1, -6, 2, 1 };
+            Assert.IsTrue(equi.DivideSum(arr, 1));
+        }
+        [Test]
+        public void SolutionTest()
+        {
+            var arr = new int[] { -1, 3, -4, 5, 1, -6, 2, 1 };
+            Assert.AreEqual(new List<int>() { 1, 3, 7 }, equi.Solution(arr));
+        }
+    }
+}
