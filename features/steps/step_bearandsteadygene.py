cca233ca3affb21e445688bbeaa6cce64d62b0c9
kusachiy <xavierernandez@gmail.com>
2016-12-12 12:33:30 +0300

    Fixed tests... again....

--- features/steps/step_bearandsteadygene.py
+++ features/steps/step_bearandsteadygene.py
@@ -5,17 +5,17 @@ from bearandsteadygene import BearAndSteadyGene
 
 @given('Create an instance of BASGKata Gene: "{gene}", Length: "{length}"')
 def step(context, length,gene):
-    context.solution = BearAndSteadyGene(int(length),gene)
+    context.solution = BearAndSteadyGene(int(length), gene)
 
 
 @then('Cortege of repetitions letters from "{gene}" is "{cortege}"')
 def step(context,gene,cortege):
-        assert_that(context.solution.calc_count_of_repetitions(gene),equal_to(toIntArray(str.split(cortege, sep=','))))
+        assert_that(context.solution.calc_count_of_repetitions(gene),equal_to(toIntArray(cortege.split(','))))
 
 
 @then('Cortege of excesses letters from "{gene}" is "{cortege}"')
 def step(context,gene,cortege):
-        assert_that(context.solution.get_excess_elements(toIntArray(str.split(gene,sep=','))),equal_to(toIntArray(str.split(cortege, sep=','))))
+        assert_that(context.solution.get_excess_elements(toIntArray(gene.split(','))),equal_to(toIntArray(cortege.split(','))))
 
 
 @then('Cortege "{first}" contains cortege "{second}"... Is "{boolka}"')
