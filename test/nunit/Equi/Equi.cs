6f9017c77e13e2044a8ff3334c8954030f4de066
Vega501 <timon141119@mail.ru>
2016-12-16 18:09:33 +0300

    Добавил решение для EquiKata.

---
+++ test/nunit/Equi/Equi.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.Equi
+{
+    class Equi
+    {
+        public int[] Solution(int[] arr)
+        {
+            int[] result = new int[arr.Length];
+            int left = 0;               //  Левая граница массива 
+            int rigth = 0;              //  Правая граница массива 
+            int sum = 0;                //  Сумма элементов массива
+            int j = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            do
+            {
+                if (rigth != 0 || sum != 0)
+                {
+                    sum -= arr[j];
+                    rigth = sum;
+                    if (left == rigth)
+                    {
+                        result[j] = 1;
+                    }
+                    else
+                    {
+                        result[j] = 0;
+                    }
+                    left += arr[j];
+                    j++;
+                }
+                else
+                {
+                    break;
+                }
+                continue;
+            } while (left != rigth);
+            return result;
+        }
+    }
+}
---
+++ test/nunit/Equi/EquiTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Katas.Equi
+{
+    [TestFixture]
+    class EquiTests
+    {
+        Equi et = new Equi();
+        [Test]
+        public void Exist()
+        {
+            Assert.IsNotNull(et);
+        }
+        [Test]
+        public void SolutionTest()
+        {
+            int[] arr = { -1, 3, -4, 5, 1, -6, 2, 1 };
+            Assert.AreEqual(new int[] { 0, 1, 0, 1, 0, 0, 0, 1}, et.Solution(arr));
+        }
+    }
+}
