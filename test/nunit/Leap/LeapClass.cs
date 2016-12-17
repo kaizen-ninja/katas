a1f2dc172ab334d77d14292d9418b557ab76e600
Pavel <merixman@yandex.ru>
2016-12-17 15:18:18 +0300

    Added LeapYear Solution

---
+++ test/nunit/Leap/LeapClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.NUnit
+{
+    class LeapClass
+    {
+        int year;
+        public LeapClass(int year)
+        {
+            this.year = year;
+        }
+        public bool IsLeap()
+        {
+            if((year % 400 == 0 && year % 4 == 0) || year % 100 != 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ test/nunit/Leap/LeapTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Katas.NUnit
+{
+    class LeapTests
+    {
+        LeapClass lc = new LeapClass(1992);
+        LeapClass newlc = new LeapClass(2000);
+        LeapClass newlc2 = new LeapClass(1900);
+
+        [Test]
+        public void LeapTest()
+        {
+            Assert.AreEqual(true, lc.IsLeap());
+            Assert.IsNotNull(lc);
+        }
+
+        [Test]
+        public void LeapTest2()
+        {
+            Assert.AreEqual(true, newlc.IsLeap());
+            Assert.IsNotNull(newlc);
+        }
+
+        [Test]
+        public void LeapTest3()
+        {
+            Assert.AreEqual(false, newlc2.IsLeap());
+            Assert.IsNotNull(newlc2);
+        }
+    }
+}
