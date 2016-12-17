7d246c03b053495747812d9b0f2afdbeaaf511d1
Pavel <merixman@yandex.ru>
2016-12-17 14:11:51 +0300

    Added BinaryGap Solution

---
+++ test/nunit/Binary/BinClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.NUnit
+{
+    class BinClass
+    {
+        int x;
+        string s = "";
+        public BinClass(int x)
+        {
+            this.x = x;
+        }
+
+        public int Solution()
+        {
+            int maxcount = 0;
+            int count = 0;
+            s = ConvertToBin();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '0')
+                {
+                    count += 1;
+                }
+                if (s[i] == '1')
+                {
+                    maxcount = Math.Max(maxcount, count);
+                    count = 0;
+                }
+            }
+            return maxcount;
+        }
+
+        public string ConvertToBin()
+        {
+            while (x > 0)
+            {
+                if (x % 2 == 0)
+                {
+                    s = "0" + s;
+                }
+                else
+                    s = "1" + s;
+                x /= 2;
+            }
+            return s;
+        }
+    }
+}
---
+++ test/nunit/Binary/BinTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Katas.NUnit
+{
+    class BinTests
+    {
+        BinClass bc = new BinClass(1041);
+        BinClass newbc = new BinClass(2222);
+        BinClass newbc2 = new BinClass(1401);
+
+        [Test]
+        public void BinTest()
+        {
+            Assert.AreEqual("10000010001", bc.ConvertToBin());
+            Assert.AreEqual(5, bc.Solution());
+            Assert.IsNotNull(bc);
+        }
+
+        [Test]
+        public void BinTest2()
+        {
+            Assert.AreEqual("100010101110", newbc.ConvertToBin());
+            Assert.AreEqual(3, newbc.Solution());
+            Assert.IsNotNull(newbc);
+        }
+
+        [Test]
+        public void BinTest3()
+        {
+            Assert.AreEqual("10101111001", newbc2.ConvertToBin());
+            Assert.AreEqual(2, newbc2.Solution());
+            Assert.IsNotNull(newbc2);
+        }
+    }
+}
