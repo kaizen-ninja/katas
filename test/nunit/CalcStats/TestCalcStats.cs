a98a7532e6c1e426c657b6cd0263c6faf8e085dd
VadimVerhovskyi <tabtyjka@gmail.com>
2016-12-18 00:42:39 +0300

    Executed project "CalcStatsKatas".

---
+++ test/nunit/CalcStats/TestCalcStats.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.CalcStats
+{
+    [TestFixture]
+    public class TestCalc_Stats
+    {
+        List<double> set = new List<double>() {1,2.54,5,16,54.789 };
+        CalcStats CS = new CalcStats();
+        [Test]
+        public void TestCreatingEkz()
+        {
+            Assert.IsNotNull(CS);
+        }
+        [Test]
+        public void TestMinValue()
+        {
+            Assert.AreEqual(1, CS.MinValue(set));
+        }
+        [Test]
+        public void TestMaxValue()
+        {
+            Assert.AreEqual(54.789, CS.MaxValue(set));
+        }
+        [Test]
+        public void TestAverageValue()
+        {
+            Assert.AreEqual(15, CS.AverageValue(set));
+        }
+        [Test]
+        public void TestNumberOfElementsInTheSequence()
+        {
+            Assert.AreEqual(5, CS.NumberOfElementsInTheSequence(set));
+        }
+    }
+}
