c99477e1284cbbe1c90f585f095201fefdd0d455
jonny-novikov <novikov@amiforus.com>
2017-07-26 15:21:14 +0000

    CircularPrimes: for Nikitos

---
+++ test/nunit/CircularPrimes/CircularPrimes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Katas.CircularPrimes
+{
+    public class CircularPrimes
+    {
+        private bool IsPrimeNumber(int x)
+        {
+            if (x < 2) return false;
+            for (int i = 2; i <= x / 2; i++)
+            {
+                if (x % i == 0) return false;
+            }
+            return true;
+        }
+
+        private bool IsCircularPrimeNumber(int x)
+        {
+            // TODO: Nikitos, put here your ideas
+            return IsPrimeNumber(x);
+        }
+
+        public List<int> GetDigits(int x)
+        {
+            var digits = new List<int>();
+            // TODO: Nikitos, write loop
+            digits.Add(x % 10);
+            return digits;
+        }
+
+        public int CalcCircularPrimesCount(int n)
+        {
+            var primes = 0;
+            for (int i = 2; i < n; i++)
+            {
+                if (IsCircularPrimeNumber(i))
+                {
+                    Console.WriteLine(i);
+                    primes++;
+                }
+            }
+            return primes;
+        }
+    }
+}
---
+++ test/nunit/CircularPrimes/TestCircularPrimes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Katas.CircularPrimes
+{
+    [TestFixture]
+    public class TestCircularPrimes
+    {
+        [Test]
+        [TestCase(1, 0)]
+        [TestCase(10, 4)]
+        [TestCase(100, 13)]
+        [TestCase(1000, 25)]
+        [TestCase(10000, 33)]
+        [TestCase(100000, 43)]
+        [TestCase(1000000, 55)]
+        public void TestPrimesBelow(int n, int expected)
+        {
+            var circularPrimes = new CircularPrimes();
+            circularPrimes.CalcCircularPrimesCount(n).Should().Be(expected);
+        }
+    }
+}
